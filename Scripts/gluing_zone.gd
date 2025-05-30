072bdd634329fe99d956970502f0a2d188c942bb
Lighar <45882804+Lighar@users.noreply.github.com>
2025-05-30 18:53:53 +0200

    Background + biggersheep

--- Scripts/gluing_zone.gd
+++ Scripts/gluing_zone.gd
@@ -4,3 +4,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body is Sheep:
 		body.can_be_glued = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Sheep:
+		body.can_be_glued = false
