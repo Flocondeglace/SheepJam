cf16b0f7d54bd8cf9daa75bdb0ad027f7c6b9ae4
Lighar <45882804+Lighar@users.noreply.github.com>
2025-05-31 21:30:11 +0200

    music dialogue

---
+++ Scripts/dialoguemanager.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@onready var anim = $AnimationPlayer
+
+@onready var dialogue = $Dialogue
+
+@onready var animation_player: AnimationPlayer = $"../Dieu/AnimationPlayer"
+
+
+func _process(_delta):
+	if dialogue.message_id in [2,4,6,12]:
+		if Input.is_action_just_pressed("ui_accept"):
+			dialogue.next_message()
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		dialogue.skip_message()
+	
+func startDialogue():
+	print("start")
+	dialogue.start_dialogue()
+	animation_player.play("talk")
+
+
+func onMessageFinished():
+	dialogue.next_message()
+
+
+func onDialogueEnded():
+	animation_player.stop()
+	print("end")
