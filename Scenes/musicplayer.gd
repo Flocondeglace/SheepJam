757257e0c3a42ad501578903e62feb73c9203a1b
Lighar <45882804+Lighar@users.noreply.github.com>
2025-06-02 21:26:44 +0200

    FixedAudio

--- Scenes/musicplayer.gd
+++ Scenes/musicplayer.gd
@@ -1,24 +1,27 @@
-extends AudioStreamPlayer
+extends Node
 
+@onready var epic: AudioStreamPlayer = $epic
+@onready var normal: AudioStreamPlayer = $normal
 
 @onready var game: Node2D = $".."
 var current_progress_percentage = 0
-var min_volume = -30
+var min_volume = -25
 var max_volume = 0
 var volume_distance
 
 func _ready():
 	volume_distance = max_volume - min_volume
-	stream.set_sync_stream_volume(1,min_volume)
-	stream.set_sync_stream_volume(0,max_volume)
+
+	epic.set_volume_db(min_volume)
+	normal.set_volume_db(max_volume)
 
 func _process(_delta):
 	current_progress_percentage = clamp(game.progress_percentage,0,0.9)
 
-	if current_progress_percentage > 0.8:
-		var volume_percent = (current_progress_percentage - 0.8)/(0.9-0.8)
+	if current_progress_percentage > 0.7:
+		var volume_percent = (current_progress_percentage - 0.7)/(0.9-0.7)
 		var volume_music_1 = min_volume + volume_percent * volume_distance
 		var volume_music_0 = max_volume - volume_percent * volume_distance
 	
-		stream.set_sync_stream_volume(0,volume_music_0)
-		stream.set_sync_stream_volume(1,volume_music_1)
+		normal.set_volume_db(volume_music_0)
+		epic.set_volume_db(volume_music_1)
